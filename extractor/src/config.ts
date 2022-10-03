c54a9ab959385b01ae2f776bf993e6372ee7c825
Henrik Steen <henrist@henrist.net>
2022-10-03 07:00:04 +0200

    Move more values to env

--- extractor/src/config.ts
+++ extractor/src/config.ts
@@ -12,12 +12,12 @@ function requireEnv(name: string): string {
 
 export const FJERNVARME_USERNAME = requireEnv("FJERNVARME_USERNAME");
 export const FJERNVARME_PASSWORD = requireEnv("FJERNVARME_PASSWORD");
-export const FJERNVARME_ANLEGG_NUMMER = "123456789";
-export const FJERNVARME_KUNDE_ID = "1234";
+export const FJERNVARME_ANLEGG_NUMMER = requireEnv("FJERNVARME_ANLEGG_NUMMER");
+export const FJERNVARME_KUNDE_ID = requireEnv("FJERNVARME_KUNDE_ID");
 
 export const STROEM_USERNAME = requireEnv("STROEM_USERNAME");
 export const STROEM_PASSWORD = requireEnv("STROEM_PASSWORD");
-export const STROEM_METER_LIST = ["707057500051111111", "707057500051222222"];
+export const STROEM_METER_LIST = requireEnv("STROEM_METER_LIST").split(",");
 
 export const DATA_FILE = process.env["DATA_FILE"] ?? "data.json";
 
